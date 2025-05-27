ede51ff2c6931469cb0f3fd54dc161e7e3fcbef0
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2025-05-27 06:58:19 +0530

    routing added

--- ShoppyGlobe/src/App.jsx
+++ ShoppyGlobe/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './components/Header.jsx'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-center'>App</div>
+    <div className='text-center'>
+      <Header />
+      <Outlet />
+    </div>
   )
 }
 
--- ShoppyGlobe/src/main.jsx
+++ ShoppyGlobe/src/main.jsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import NotFound from './pages/NotFound.jsx';
+import Home from './pages/Home.jsx';
+import Cart from './pages/Cart.jsx'
+import Checkout from './pages/Checkout.jsx'
+import ProductDetail from './pages/ProductDetail.jsx'
+
+const approuter = createBrowserRouter([
+  {
+    path:'/',
+    element:<App />,
+    children:[
+      {
+        path:'/',
+        element:<Home />
+      },
+      {
+        path:'/cart',
+        element:<Cart />
+      },
+      {
+        path:'/checkout',
+        element:<Checkout />
+      },
+      {
+        path:'/productdetail',
+        element:<ProductDetail />
+      }
+    ],
+    errorElement:<NotFound />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={approuter} />
   </StrictMode>,
 )
